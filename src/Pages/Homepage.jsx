6ab08dd59ad3536d479fe6b78af155c6f83a3c21
Harshit Jaiswal <harshitjaiswal022@gmail.com>
2025-12-11 12:37:31 +0530

    3commit

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,8 +1,6 @@
 import { Hero } from "../components/Hero/Hero";
 import { About } from "../components/About/About";
-import { Events } from "../components/Events/Events";
-import { Resources } from "../components/Resources/Resources";
-import { Blog } from "../components/Blog/Blog";
+
 import { Contact } from "../components/Contact/Contact";
 import SlideOnScroll from "../components/ScrollOnScroll";
 
@@ -10,13 +8,12 @@ export default function HomePage() {
   return (
     <>
       <Hero />
-      {/* <SlideOnScroll direction="left"> */}
-      <About />
-      {/* </SlideOnScroll> */}
-      <SlideOnScroll direction="right">{/* <Events /> */}</SlideOnScroll>
-      <SlideOnScroll direction="left">{/* <Resources /> */}</SlideOnScroll>
-      <SlideOnScroll direction="right">{/* <Blog /> */}</SlideOnScroll>
-      <SlideOnScroll direction="right">{/* <Contact /> */}</SlideOnScroll>
+      <SlideOnScroll direction="left">
+        <About />
+      </SlideOnScroll>
+      <SlideOnScroll direction="right">
+        <Contact />
+      </SlideOnScroll>
     </>
   );
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,22 @@
-import React from "react";
-
-const Contact = () => {
-  return <div></div>;
-};
-
-export default Contact;
+export const Contact = () => (
+  <section className="py-20 bg-gray-900 text-white px-6">
+    <h2 className="text-4xl font-bold text-center mb-10">Contact Us</h2>
+    <form className="max-w-xl mx-auto grid gap-4">
+      <input
+        className="p-3 rounded bg-gray-800 text-white"
+        placeholder="Your Name"
+      />
+      <input
+        className="p-3 rounded bg-gray-800 text-white"
+        placeholder="Email"
+      />
+      <textarea
+        className="p-3 rounded bg-gray-800 text-white"
+        placeholder="Message"
+      ></textarea>
+      <button className="py-3 bg-teal-500 text-black font-semibold rounded hover:bg-teal-400 transition">
+        Send
+      </button>
+    </form>
+  </section>
+);
